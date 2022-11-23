2db4e74db243afaf72fad676497662e1a7097d8a
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-23 15:27:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import AppTitle from './components/AppTitle.vue'
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped>
+</style>
